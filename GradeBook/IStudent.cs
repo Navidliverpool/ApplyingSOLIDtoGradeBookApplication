9cdc0737be487487e3799c226b49b6286ba13e3a
Navid Khodaveisi <navidliverpool@yahoo.com>
2021-06-19 20:45:57 +0100

    refactoring the IStudent interface commit number 1

--- GradeBook/IStudent.cs
+++ GradeBook/IStudent.cs
@@ -7,5 +7,8 @@ namespace GradeBook
     public interface IStudent
     {
        void AddGrade(double grade, double minimumGrade, double maximunGrade);
+       void RemoveGrade(double grade);
+       void AddStudent(Student student);
+       void RemoveStudent(string name);
     }
 }
